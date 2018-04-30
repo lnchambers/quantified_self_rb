ae201ae6ebfef7d9e9046fe0e4a60e758f88dc6a
Luke Chambers <lukenatchambers@gmail.com>
2018-04-30 13:59:07 -0600

    Build test for food show

--- spec/requests/food_request_spec.rb
+++ spec/requests/food_request_spec.rb
@@ -14,7 +14,21 @@ describe "Food Requests" do
 
     expect(parsed.count).to eq(3)
     expect(parsed[0][:name]).to eq(@food.name)
+    expect(parsed[0][:calories]).to eq(@food.calories)
     expect(parsed[1][:name]).to eq(@food_2.name)
+    expect(parsed[1][:calories]).to eq(@food_2.calories)
     expect(parsed[2][:name]).to eq(@food_3.name)
+    expect(parsed[2][:calories]).to eq(@food_3.calories)
+  end
+
+  it "GET /api/v1/foods/:id returns one food matching the id or 404 if not found" do
+    get "/api/v1/foods/2"
+
+    expect(response).to be_success
+    parsed = JSON.parse(response.body, symbolize_names: true)
+
+    expect(parsed[:name]).to eq(@food_2.name)
+    expect(parsed[:calories]).to eq(@food_2.calories)
+    expect(parsed[:id]).to eq(@food_2.id)
   end
 end
